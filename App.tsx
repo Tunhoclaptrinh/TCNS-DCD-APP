0929e80ce65d5bed85a36e902134e62d086db7e2
Hng2725 <nguyenviethung270205@gmail.com>
2026-03-02 21:12:01 +0700

    fix login

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './src/store';
-import RootNavigator from './src/navigation/RootNavigator';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from "react";
+import { Provider, useSelector } from "react-redux";
+import { store } from "./src/store";
+import RootNavigator from "./src/navigation/RootNavigator";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { RootState } from "./src/store";
+
+function RootContent() {
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated,
+  );
+  return <RootNavigator isAuthenticated={isAuthenticated} />;
+}
 
 export default function App() {
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <RootNavigator />
+        <RootContent />
       </SafeAreaProvider>
     </Provider>
   );
